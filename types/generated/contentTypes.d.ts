5e93a0eedc5513a6f9b31451ec14765cc657f8e6
krsticzoran <zorankrstic81@gmail.com>
2025-09-19 18:04:34 +0200

    feat(blog): add Blog collection type with mainImage and content fields

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -384,7 +384,6 @@ export interface ApiBlogBlog extends Struct.CollectionTypeSchema {
     draftAndPublish: true;
   };
   attributes: {
-    content: Schema.Attribute.Blocks & Schema.Attribute.Required;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
